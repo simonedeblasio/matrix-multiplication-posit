e5502ccaf75ee69757095f491cce0bdfff43cb04
Fredrik Karpers <fkarpers@kth.se>
2020-04-03 17:42:19 +0200

    Fixade så att den tar argument och loopar igenom en massa saker :=)

--- code/matrix.py
+++ code/matrix.py
@@ -4,19 +4,19 @@ import softfloat as sf
 import random as random
 import numpy as np
 import csv
+import sys
 
 # Standard size
 matrix_size = (2, 2)
 
 
-
-def main(size):
+def main(size, interval):
     # Float 64
     global matrix_size
     matrix_size = size
 
-    m1float64 = fillWithRandom()
-    m2float64 = fillWithRandom()
+    m1float64 = fillWithRandom(interval)
+    m2float64 = fillWithRandom(interval)
     resultfloat64 = fillWithZeros("float64")
 
     # Posit 32
@@ -37,7 +37,6 @@ def main(size):
     convertFromFloat64Matrix(m1float64, m1float32, "float32")
     convertFromFloat64Matrix(m2float64, m2float32, "float32")
 
-
     # Floating point multiplication
     matrixMultiplication(m1float64, m2float64, resultfloat64, "float64")
     matrixMultiplication(m1posit32, m2posit32, resultposit32, "posit32")
@@ -45,7 +44,7 @@ def main(size):
 
     return [sumDiffOfMatrixes(resultposit32, resultfloat64), sumDiffOfMatrixes(
         resultfloat32, resultfloat64
-        )]
+    )]
 
 
 def convertFromFloat64Matrix(randomMatrix, targetMatrix, floatFormat):
@@ -63,21 +62,21 @@ def fillWithZeros(floatFormat):
     if floatFormat == "float64":
         return [[0 for i in range(matrix_size[0])] for j in range(
             matrix_size[1]
-            )]
+        )]
     elif floatFormat == "posit32":
         return [[sp.posit32(0) for i in range(matrix_size[0])] for j in range(
             matrix_size[1]
-            )]
+        )]
     elif floatFormat == "float32":
         return [[sf.float32(0) for i in range(matrix_size[0])] for j in range(
             matrix_size[1]
-            )]
-
-
-def fillWithRandom():
-    return [[random.random() for i in range(matrix_size[0])] for j in range(
-        matrix_size[1]
         )]
+
+
+def fillWithRandom(interval):
+    return [[np.random.uniform(interval*-1, interval) for i in range(matrix_size[0])] for j in range(
+        matrix_size[1]
+    )]
 
 
 def matrixMultiplication(m1, m2, result, floatFormat):
@@ -106,11 +105,18 @@ def sumDiffOfMatrixes(m1, m2):
 
     return np.sum(diff)
 
+for i in range(8):
+    pow = 2**i
+    # This writes return of main to csv files
+    with open( str(sys.argv[1]) + "data" + str(pow) + "x" + str(pow) + ".csv", "a", newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(["index", "Posit32 error", "Float32 error", "Matrix size", "Random interval"])
+        for x in range(1000):
+            random_interval = float(sys.argv[1])
+            result = main((pow, pow), random_interval)
+            writer.writerow([x, result[0], result[1], pow, random_interval])
 
-# This writes return of main to csv files
-with open("data2x2test.csv", "a", newline='') as csvfile:
-    writer = csv.writer(csvfile)
-    writer.writerow(["index", "Posit32 error", "Float32 error"])
-    for x in range(100):
-        result = main((4, 4))
-        writer.writerow([x, result[0], result[1]])
+
+def uniform(self, a, b):
+    "Get a random number in the range [a, b) or [a, b] depending on rounding."
+    return a + (b-a) * self.random()
