d094f15af7195ba8a570090b1314dc21aa0c9f18
Fredrik Karpers <fkarpers@kth.se>
2020-04-29 19:28:07 +0200

    Added script to execute tests

---
+++ code/execute.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+python3.6 matrix.py 1						&
+python3.6 matrix.py 0.01					&
+python3.6 matrix.py 100						&
+python3.6 matrix.py 0.0001					&
+python3.6 matrix.py 10000					&
+python3.6 matrix.py 0.000001				&
+python3.6 matrix.py 1000000					&
+python3.6 matrix.py 0.00000001				&
+python3.6 matrix.py 100000000				&
+python3.6 matrix.py 0.0000000001			&
+python3.6 matrix.py 10000000000				&
+python3.6 matrix.py 0.000000000001			&
+python3.6 matrix.py 1000000000000			&
+python3.6 matrix.py 0.00000000000001		&
+python3.6 matrix.py 100000000000000			&
+python3.6 matrix.py 0.0000000000000001		&
+python3.6 matrix.py 10000000000000000		&
